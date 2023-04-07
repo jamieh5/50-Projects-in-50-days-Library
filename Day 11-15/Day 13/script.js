be790c2c358a27d6b3077431f9ea69608d12f377
Jamie Hoyer <hoyer307@gmail.com>
2023-04-07 11:51:12 +0200

    finished day 13

---
+++ Day 11-15/Day 13/script.js
@@ -0,0 +1,19 @@
+const textArea = document.querySelector('.input-field')
+const btn = document.querySelector('button')
+const containerChoices = document.querySelector('.container-choices')
+
+let words = []
+
+btn.addEventListener('click', function () {
+  const str = textArea.value
+  words = str.split(',')
+  createSpan()
+})
+
+function createSpan() {
+  words.forEach(function (e) {
+    const element = document.createElement('span')
+    element.innerHTML = e
+    document.body.appendChild(element)
+  })
+}
